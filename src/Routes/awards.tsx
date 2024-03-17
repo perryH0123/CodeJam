8012fda2e032a39886d9adf6f905d2ee2d1a5b88
Siddhant <siddhantkhapra101@gmail.com>
2024-03-17 12:55:32 -0700

    Awards

--- src/Routes/awards.tsx
+++ src/Routes/awards.tsx
@@ -11,22 +11,22 @@ const Awards = () => {
         <h2>Awards</h2>
         <div id="awards-container">
             {awards[0] && <div id="1st">
-                <h2>1st Place</h2>
+                <h2>Most Creative</h2>
                 <img src={awards[0]?.img || DefaultImage} alt=""/>
                 <h3>{awards[0].team}</h3>
             </div>}
             {awards.length > 1 && awards[1] && <div id="2nd">
-                <h2>2nd Place</h2>
+                <h2>Technical Achievement</h2>
                 <img src={awards[1]?.img || DefaultImage} alt=""/>
                 <h3>{awards[1].team}</h3>
             </div>}
             {awards.length > 2 && awards[2] && <div id="3nd">
-                <h2>3rd Place</h2>
+                <h2>Finalist Project</h2>
                 <img src={awards[2]?.img || DefaultImage} alt=""/>
                 <h3>{awards[2].team}</h3>
             </div>}
             {awards.length > 3 && awards[3] && <div id="honorable">
-                <h2>Honorable Mention</h2>
+                <h2>Best Overall</h2>
                 <img src={awards[3]?.img || DefaultImage} alt=""/>
                 <h3>{awards[3].team}</h3>
             </div>}
